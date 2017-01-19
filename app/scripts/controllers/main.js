0b31251110ceb96dd6f9ac0c3714450ae0bcc2fd
James Gudhlanga <jimmyneds@gmail.com>
2017-01-19 09:42:32 +0200

    Project First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularappApp
+ */
+angular.module('angularappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
