3cdba8ea16656fbbf237c911aeae2203eb805181
James Gudhlanga <jimmyneds@gmail.com>
2017-01-25 09:43:01 +0200

    Update project.js

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -7,7 +7,7 @@
  * Controller of the angularappApp
  */
 angular.module('angularappApp')
-    .controller('ProjectCtrl', function ($scope, ProjectService, $cookies, $location, $routeParams, $route, $templateCache )  {
+    .controller('ProjectCtrl', function ($scope, ProjectService, $cookies, $location, $routeParams, $route )  {
 
         var config = {
             headers: {
@@ -24,30 +24,39 @@ angular.module('angularappApp')
 
         //initialise the post data object
         $scope.projectData = { };
-        $scope.postProject = function()
-        {
-            ProjectService.postProject($scope.projectData);
-            //add the newly added project to the projects
-            $scope.projects.push($scope.projectData);
-            $location.path("/project");
-        }
-
+        //modal specific variables
+        
         //get the project to edit only if the routeParam pk is set
 
         if ($routeParams.pk) {
             var OnComplete = function(data) {
-                $scope.project = data;
+                $scope.projectData = data;
             }
+            $scope.header = 'Edit Project';
+             $scope.button = 'Update Project';
             var pk = $routeParams.pk;
-            $scope.project = ProjectService.getProject(pk).then(OnComplete);
+            $scope.projectData = ProjectService.getProject(pk).then(OnComplete); 
+            $scope.postProject = function()
+            {      
+                ProjectService.updateProject($scope.projectData);
+                //redirect to the projects page
+                $location.path("/project");
+            }
+        }
+        else
+        {
+            $scope.header = 'Create New Project';
+            $scope.button = 'Save Project';
+
+            $scope.postProject = function()
+            {
+                ProjectService.postProject($scope.projectData);
+                //add the newly added project to the projects
+                $scope.projects.push($scope.projectData);
+                $location.path("/project");
+            }
         }
 
-        $scope.updateProject = function()
-        {
-            ProjectService.updateProject($scope.project);
-            //redirect to the projects page
-            $location.path("/project");
-        }
 
         $scope.deleteProject = function(id)
         {
@@ -63,4 +72,5 @@ angular.module('angularappApp')
             //redirect to the projects page
             $location.path("/project");
         }
-    });
+
+        });
